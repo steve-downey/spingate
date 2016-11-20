10e35e3b45939dedf5df80be781776ae8ff9ecc8
Steve Downey <sdowney@gmail.com>
2016-11-20 17:41:58 -0500

    Add reference tests
    Add more reference tests for batch.

--- tests/batch.t.cpp
+++ tests/batch.t.cpp
@@ -77,11 +77,54 @@ TEST_F(BatchTest, runArgTest)
     int i = 0;
     batch.add([&i](int k){ i = k;}, 1);
 
+    EXPECT_EQ(i, 0);
+
+    batch.run();
+
+    EXPECT_EQ(i, 1);
+}
+
+TEST_F(BatchTest, runRefArgTest)
+{
+    Batch batch;
+    int i = 0;
+    batch.add([](int& k){ k = 2;}, std::ref(i));
+
+    EXPECT_EQ(i, 0);
+
+    batch.run();
+
+    EXPECT_EQ(i, 2);
+}
+
+TEST_F(BatchTest, runRefArgTest2)
+{
+    Batch batch;
+    int i = 0;
+    batch.add([](int& k, int j){ k = j;}, std::ref(i), 3);
+
     EXPECT_EQ(0, i);
 
     batch.run();
 
-    EXPECT_EQ(1, i);
+    EXPECT_EQ(3, i);
+}
+
+std::string someString() {
+    return std::string("four");
+}
+
+TEST_F(BatchTest, runRefArgTest3)
+{
+    Batch batch;
+    int i = 0;
+    batch.add([](int& k, std::string j){ k = j.length();}, std::ref(i), someString());
+
+    EXPECT_EQ(0, i);
+
+    batch.run();
+
+    EXPECT_EQ(4, i);
 }
 
 }
